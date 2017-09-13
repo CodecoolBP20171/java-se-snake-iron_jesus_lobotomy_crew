799c1b275b16506a226861feebdc75bf6a51b4c5
Annamaria Sojnoczki <soj.annam@gmail.com>
2017-09-13 12:01:08 +0200

    add game over popup

--- src/com/codecool/snake/Game.java
+++ src/com/codecool/snake/Game.java
@@ -4,9 +4,16 @@ import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.entities.enemies.SimpleEnemy;
 import com.codecool.snake.entities.powerups.SimplePowerup;
 import com.codecool.snake.entities.snakes.SnakeHead;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Game extends Pane {
 
@@ -52,6 +59,43 @@ public class Game extends Pane {
         new SimplePowerup(this);
         new SimplePowerup(this);
     }
+
+
+    //Show Congratulation popup window when player won
+    public static void showPopup(){
+
+        // Creating pop up window
+        Stage newStage = new Stage();
+        VBox winPopup = new VBox();
+        Button exit = new Button();
+
+        // setting button function
+        exit.setText("Exit");
+        exit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.exit(1);
+            }
+        });
+
+        // Adding text
+        Text gameOver = new Text("Game Over");
+        Text score = new Text ("Your score is "+ Globals.score);
+        winPopup.getChildren().add(gameOver);
+        winPopup.getChildren().add(score);
+
+        // Setting scene
+        Scene stageScene = new Scene(winPopup, 200, 100);
+        newStage.setScene(stageScene);
+
+        // Styling
+
+
+        // Show
+        winPopup.getChildren().add(exit);
+        newStage.show();
+    }
+
 
 }
 
